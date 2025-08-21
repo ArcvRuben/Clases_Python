6b1e368cf1829212ee8da21ac547bf62e8c46ecf
ArcvRuben <127159003+ArcvRuben@users.noreply.github.com>
2025-08-21 22:59:39 +0000

    Lab3 corregido

--- lab3/main.py
+++ lab3/main.py
@@ -1,9 +1,15 @@
+# main.py
 from src.operaciones import suma, division
 
 def main():
     print("Resultado suma:", suma(5, 7))
     print("Resultado división válida:", division(10, 2))
-    print("Resultado división por cero :", division(10, 0))
+    
+    try:
+        resultado = division(10, 0)
+        print("Resultado división por cero:", resultado)
+    except ValueError as e:
+        print("Error en división:", e)
 
 if __name__ == "__main__":
     main()
